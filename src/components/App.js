c9d19b6e2504c642f19c158ca3c650476a7273e5
Prasanth-1402 <prasanthshanmugam1402@gmail.com>
2020-11-23 15:53:02 +0530

    Check

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { Component, useState, Fragment } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
+
 class Timer extends React.Component {
   constructor(props) {
     super(props);
@@ -7,18 +8,23 @@ class Timer extends React.Component {
     this.timerInterval = null;
     this.keyListener = this.keyListener.bind(this);
     this.gameStart = this.gameStart.bind(this); 
+    // console.log(this.state.started +" = STARTED(construcotr) , time = "+this.state.time);
   }
 
   keyListener(eve){
+    // console.log(this.state.started +" = started (listener), time = "+this.state.time);
     if(this.state.started){
       if(eve.keyCode === 37){
-        this.setState({x: this.state.x - 5})
-      }else if(eve.keyCode === 38){
-        this.setState({y: this.state.y - 5})
-      }else if(eve.keyCode === 39){
-        this.setState({x: this.state.x + 5})
-      }else if(eve.keyCode === 40){
-        this.setState({y: this.state.y + 5})
+        this.setState({x: this.state.x - 5});
+      }
+      else if(eve.keyCode === 38){
+        this.setState({y: this.state.y - 5});
+      }
+      else if(eve.keyCode === 39){
+        this.setState({x: this.state.x + 5});
+      }
+        else if(eve.keyCode === 40){
+        this.setState({y: this.state.y + 5});
       } 
     }
   }
@@ -31,7 +37,7 @@ class Timer extends React.Component {
       () =>  this.setState({time : this.state.time + 1}),
        1 * 1000
       );
-    document.addEventListener("keydown", this.keyListener);
+    document.addEventListener('keydown', this.keyListener);
   };
   
   componentDidMount() { }
@@ -43,20 +49,23 @@ class Timer extends React.Component {
     }
   }
 
-  componentWillUnmount() {  }
+  componentWillUnmount() {}
 
 
 
   render() {
     return (
             <>
-              {!this.state.started ? 
-                   (
-                    <button className="start" onClick={this.gameStart}> Start Timer </button>
-                  ) : ( <>
-                      <div className="ball" style = {{ left: this.state.x+"px" , top: this.state.y+"px"}}></div>
-                      <div className="hole"></div>
-                      <div className="heading-timer">{this.state.time}</div>
+              {!this.state.started ? (
+                    <button className = "start" onClick={this.gameStart}> Start Timer </button>
+                  ) : (
+                     <>
+                      <div
+                       className = "ball" 
+                       style = {{ left: this.state.x+"px" , top: this.state.y+"px"}}> 
+                      </div>
+                      <div className = "hole" style = {{left:"250px" , top: "250px"}}></div>
+                      <div className = "heading-timer">{this.state.time}</div>
                     </>
               )}
             </>
